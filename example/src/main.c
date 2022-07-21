e57ac5d1e1ca5cb4eeb046227fb025ffd803bedf
Coenicorn <coenicorn.blauwzee@gmail.com>
2022-07-21 22:03:17 +0200

    removed main.c

---
+++ example/src/main.c
@@ -0,0 +1,29 @@
+#include <astar.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main()
+{
+	// variable declerations are optional, you can just pass in the raw values, it's just cool af
+	const int w = 10, h = 10, num_obst = 1;
+
+	// **IMPORTANT**
+	srand(time(NULL));
+
+	Grid *g = getRandomGrid(w, h, num_obst);
+
+	// completely optional
+	printGrid(g);
+
+	int fail = pathfind(g, 0, 0, w-1, h-1, NULL);
+
+	// completely optional
+	if (!fail)
+		printGrid(g);
+	
+	// **VERY IMPORTANT** Always free the grid
+	freeGrid(g);
+
+	return 0;
+}
