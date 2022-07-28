a17109b1d50f69c7c8e193e990223930ce5a107d
Coenicorn <coenicorn.blauwzee@gmail.com>
2022-07-28 19:35:44 +0200

    updated astar.h

--- lib_files/astar.h
+++ lib_files/astar.h
@@ -60,14 +60,13 @@ void printGrid(Grid *g);
 int isValidPosition(Grid *g, int x, int y);
 
 /** Get a generated path from input variables
- * @param g The grid to do the pathfinding in
+ * @param g The grid to do the pathfinding in; THIS GRID GETS MODIFIED
  * @param startX The start position x
  * @param startY The start position y
  * @param goalX The goal position x
  * @param goalY The goal position y
  * @param path_out A predefined path variable of length MAX_PATH_LENGTH, pass NULL if you don't want an output path
  * @returns Integer for program success: 1 for error, 0 for no errors
- * @note You can define a path with Cell *path[MAX_PATH_LENGTH] (name doesn't have to be path) and just pass it to the function like (getPath(... , path)
  * */
 int pathfind(Grid *g, int startX, int startY, int goalX, int goalY, Cell *path_out[MAX_PATH_LENGTH]);
 
